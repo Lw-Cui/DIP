cb05bf7a029e1865697e3404c9b48d97faec677e
lw <cui@hellolw.com>
2016-05-07 09:12:31 +0800

    add algs 3

--- test.cpp
+++ test.cpp
@@ -11,6 +11,7 @@ namespace {
   const int LEN = 9;
   const int xp[LEN] = { 1, 1, 1, 0, 0, 0, -1, -1, -1};
   const int yp[LEN] = { 1, -1, 0, 1, -1, 0, 1, -1, 0};
+  const int THREADHOLD = 20, S_THREADHOLD = 5;
 }
 
 unsigned char getMaskColor(CImg<float> &p, int x, int y) {
@@ -18,7 +19,7 @@ unsigned char getMaskColor(CImg<float> &p, int x, int y) {
   for (int k = 0; k < p.spectrum(); k++)
     sum += p(x, y, k);
   // if the pixel appears white, then the mask is BLACK
-  if (sum > 20) return BLACK;
+  if (sum > THREADHOLD) return BLACK;
   else return WHITE;
 }
 
@@ -32,8 +33,19 @@ unsigned char getMaskColor2(CImg<float> &p, int x, int y) {
   }
   sort(v.begin(), v.end());
   // if the pixel appears black, then the mask is WHITE
-  if (v[LEN / 3 * 2] < 5) return WHITE;
-  else if (v[LEN / 3 * 2] < 20) return (WHITE + BLACK) / 2;
+  if (v[LEN / 3 * 2] < S_THREADHOLD) return WHITE;
+  else if (v[LEN / 3 * 2] < THREADHOLD) return (WHITE + BLACK) / 2;
+  else return BLACK;
+}
+
+unsigned char getMaskColor3(CImg<float> &p, int x, int y) {
+  int sum = 0;
+  for (int i = 0; i < LEN; i++)
+    for (int k = 0; k < p.spectrum(); k++)
+      sum += p(x + xp[i], y + y[p], k);
+  sum /= (LEN * p.spectrum());
+  // if the pixel appears black, then the mask is WHITE
+  if (sum < THREADHOLD) return WHITE;
   else return BLACK;
 }
 
@@ -51,12 +63,14 @@ int main() {
     char filename[100];
     sprintf(filename, "bl%d.jpg", i);
     CImg<float> src(filename);
-    CImg<float> tmp = (src - 100).cut(BLACK, WHITE).blur(2);
-    CImg<float> mask = getMask(tmp, getMaskColor),
-       mask2 = getMask(tmp, getMaskColor2);
-    (src,
+    CImg<float> blurImg = (src - 100).cut(BLACK, WHITE).blur(2);
+    CImg<float> mask = getMask(blurImg, getMaskColor),
+       mask2 = getMask(blurImg, getMaskColor2),
+       mask3 = getMask(blurImg, getMaskColor3);
+    (src, blurImg,
        mask.operator+(src).cut(BLACK, WHITE), 
-       mask2.operator+(src).cut(BLACK, WHITE)).display();
+       mask2.operator+(src).cut(BLACK, WHITE),
+       mask3.operator+(src).cut(BLACK, WHITE)).display();
   }
   return 0;
 }
